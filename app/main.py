5d28a71f7647c7dd15350256459440981909e431
Oleksii Drenov <leshadrenev@gmail.com>
2025-04-06 16:56:47 +0300

    changed variable name in the loop

--- app/main.py
+++ app/main.py
@@ -11,11 +11,11 @@ def create_person_list(people: list) -> list:
 
     friends = [Person(friend["name"], friend["age"]) for friend in people]
 
-    for i in people:
-        instance_person = Person.people[i["name"]]
-        if i.get("wife"):
-            instance_person.wife = Person.people.get(i["wife"])
-        if i.get("husband"):
-            instance_person.husband = Person.people.get(i["husband"])
+    for person_dict in people:
+        instance_person = Person.people[person_dict["name"]]
+        if person_dict.get("wife"):
+            instance_person.wife = Person.people.get(person_dict["wife"])
+        if person_dict.get("husband"):
+            instance_person.husband = Person.people.get(person_dict["husband"])
 
     return friends
